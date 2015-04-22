0d491a9366d94120c9e2bf2fa70b445027519684
Sandy Walsh <sandy.walsh@rackspace.com>
2015-04-22 15:00:34 -0700

    Switch to argparse for better client libary support.
    Docs changed too to reflect better count support (--count).
    Removed v2 api implementation example (Archive resource). We
    can always revisit once we nail down how archive retrieval
    will work.
    
    Change-Id: I79d1929a14fe51d36ecc5c54707902d5e772ad89

--- klugman/v1.py
+++ klugman/v1.py
@@ -14,76 +14,105 @@
 # limitations under the License.
 
 
-import base
-import json
-import jsonutil
+import argparse
 import prettytable
 import sys
 
-from docopt import docopt
-
-
-def check_state(state):
-    if state and state not in ["active", "firing", "expiring", "error",
-                               "expire_error", "completed",
-                               "retry_fire", "retry_expire"]:
-        print "Invalid state. %s not one of 'active, firing, " \
-              "expiring, error, expire_error, completed, " \
-              "retry_fire or retry_expire" % state
-        sys.exit(1)
+import base
 
 
 class Streams(object):
-    """usage:
-        klugman.py streams [options]
+    def __init__(self, url, subparser=None):
+        self.url = url
+        if subparser:
+            parser = subparser.add_parser('streams')
+            parser.add_argument('--name', metavar='trigger_name',
+                 help='Return streams of type trigger_name.')
+            parser.add_argument('--from', metavar='datetime', dest='_from',
+                 help='Return streams last updated after datetime')
+            parser.add_argument('--to', metavar='datetime',
+                 help='Return streams last updated before datetime')
+            parser.add_argument('--traits', metavar='trait_list',
+                 help='Return streams with specific distinguishing traits.')
+            parser.add_argument('--details', action='store_true',
+                   default=False, help='Return full event details.')
+            parser.add_argument('--state', choices=['active', 'firing',
+                   'expiring', 'error', 'expire_error', 'completed',
+                   'retry_fire', 'retry_expire'],
+                   help='Only return streams in this state.')
 
-      options:
-      --id <id>
-                get stream with id
-      --details
-                return events with each stream
-      --state <state>
-                return streams in state
-      --older_than <datetime>
-                list streams where first_event < <datetime>
-      --younger_than <datetime>
-                list streams where last_event > <datetime>
-      --trigger_name <name>
-                list streams with given trigger definition
-      --distinguishing_traits <dtraits>
-                list stream with specific distinguishing traits
+            group = parser.add_mutually_exclusive_group()
+            group.add_argument('--count', action='store_true',
+                   default=False,
+                   help='Return a count of streams matching filter criteria.')
+            group.add_argument('--id', metavar='stream_id', dest='stream_id',
+                   help='Return a single specific stream by id.')
 
-      Stream states:
-      collecting - collecting events
-      ready - ready for processing
-      triggered - being processed
-      processed - processing completed
-      error - pipeline processing failed
-      commit_error - pipeline result commit failed
+    def get_streams_count(self, from_datetime=None, to_datetime=None,
+                       traits=None, state=None, name=None, debug=False):
+        if traits:
+            traits = ",".join(["%s:%s" % item for item in traits.items()])
+        cmd = "streams/count"
+        params = base.remove_empty({'older_than': from_datetime,
+                                    'younger_than': to_datetime,
+                                    'name': name,
+                                    'state': state,
+                                    'distinguishing_traits': traits})
 
-      Distinguishing trait format:
-      "trait:value;trait:value;..."
-    """
+        return base.get(self.url, cmd, params, debug=debug)
 
-    def cmdline(self, version, cmdline):
-        arguments = docopt(Streams.__doc__, argv=cmdline)
-        debug = version.base_args['--debug']
-        if debug:
-            print arguments
+    def get_streams(self, from_datetime=None, to_datetime=None,
+                  traits=None, name=None, stream_id=None, debug=False,
+                  state=None, details=None):
+        if stream_id:
+            params = base.remove_empty({'details': details})
+            return base.get(self.url, "streams/%s" % stream_id, params,
+                            debug=debug)
 
-        response = self.do_streams(version, arguments)
-        # Handle cmdline output here, not in do_foo()
-        raw_rows = response.json(object_hook=jsonutil.object_hook)
+        if traits:
+            traits = ",".join(["%s:%s" % item for item in traits.items()])
+        cmd = "streams"
+        params = base.remove_empty({'older_than': from_datetime,
+                                    'younger_than': to_datetime,
+                                    'name': name,
+                                    'details': details,
+                                    'state': state,
+                                    'distinguishing_traits': traits})
 
-        if debug:
-            print json.dumps(raw_rows, indent=2)
+        return base.get(self.url, cmd, params, debug=debug)
 
-        # TODO(sandy): This should come from the server-issued
-        # schema at some point.
+    def _cmdline(self, arguments):
+        _from = arguments._from
+        _to = arguments.to
+        _name = arguments.name
+        _traits = arguments.traits
+        _debug = arguments.debug
+        _state = arguments.state
+        _details = arguments.details
+
+        trait_dict = None
+        if _traits:
+            trait_pairs = _traits.split(',')
+            trait_dict = dict([tuple(item.split(':')) for item in trait_pairs])
+
+        if arguments.count:
+            rows = self.get_streams_count(from_datetime=_from, to_datetime=_to,
+                                        name=_name, traits=trait_dict,
+                                        state=_state, debug=_debug)
+            print rows
+            keys = ['count']
+            base.dump_response(keys, rows)
+            return
+
+        _stream_id = arguments.stream_id
+        rows = self.get_streams(from_datetime=_from, to_datetime=_to,
+                                name=_name, traits=trait_dict,
+                                details=_details, state=_state,
+                                stream_id=_stream_id, debug=_debug)
 
         keys = ['id', 'state', 'name', 'first_event', 'last_event',
                 'fire_timestamp', 'expire_timestamp']
-        for row in raw_rows:
+        for row in rows:
             x = prettytable.PrettyTable(["Section", "Property", "Value"])
             for key in keys:
                 x.add_row(["Stream", key, row.get(key)])
@@ -105,209 +134,105 @@ class Streams(object):
                     print x
 
 
-    def do_streams(self, version, arguments):
-        sid = arguments.get('--id')
-        state = arguments.get('--state')
-        older = arguments.get('--older_than')
-        younger = arguments.get('--younger_than')
-        trigger = arguments.get('--trigger_name')
-        traits = arguments.get('--distinguishing_traits')
-        details = arguments.get('--details')
-
-        check_state(state)
-        cmd = "streams"
-        if sid:
-            cmd = "streams/%s" % sid
-            return base.get(version.base_url, cmd, {'details': details})
-
-        params = base.remove_empty({'state': state,
-                                    'older_than': older,
-                                    'younger_than': younger,
-                                    'trigger_name': trigger,
-                                    'distinguishing_traits': traits,
-                                    'details': details})
-
-        return base.get(version.base_url, cmd, params)
-
-
-class NumStreams(object):
-    """usage:
-        klugman.py num-streams [options]
-
-      options:
-      --state <state>
-                return streams in state
-      --older_than <datetime>
-                list streams older than datetime
-      --older_than <datetime>
-                list streams where first_event < <datetime>
-      --younger_than <datetime>
-                list streams where last_event > <datetime>
-      --distinguishing_traits <dtraits>
-                list stream with specific distinguishing traits
-
-      Stream states:
-      active = collecting events
-      firing = about to process events
-      expiring = about to expire stream
-      error = pipeline processing failed
-      expire_error = pipeline expiry failed
-      completed = stream processing completed
-      retry_fire = re-attempt pipeline firing
-      retry_expire = re-attempt pipeline expiry
-
-      Distinguishing trait format:
-      "trait:value;trait:value;..."
-    """
-
-    def cmdline(self, version, cmdline):
-        arguments = docopt(NumStreams.__doc__, argv=cmdline)
-        debug = version.base_args['--debug']
-        if debug:
-            print arguments
-
-        response = self.do_stream_count(version, arguments)
-        raw_rows = response.json(object_hook=jsonutil.object_hook)
-
-        keys = ['count']
-        base.dump_response(keys, raw_rows)
-
-    def do_stream_count(self, version, arguments):
-        state = arguments.get('--state')
-        older = arguments.get('--older_than')
-        younger = arguments.get('--younger_than')
-        trigger = arguments.get('--trigger_name')
-        traits = arguments.get('--distinguishing_traits')
-
-        check_state(state)
-
-        cmd = "streams/count"
-        params = base.remove_empty({'state': state,
-                                    'older_than': older,
-                                    'younger_than': younger,
-                                    'trigger_name': trigger,
-                                    'distinguishing_traits': traits})
-
-        return base.get(version.base_url, cmd, params)
-
-
 class Events(object):
-    """usage:
-        klugman.py events [options]
+    def __init__(self, url, subparser=None):
+        self.url = url
+        if subparser:
+            parser = subparser.add_parser('events')
+            parser.add_argument('--name', metavar='event_name',
+                             help='Return events of type event_name.')
+            parser.add_argument('--from', metavar='datetime', dest='_from',
+                             help='Return events generated before datetime')
+            parser.add_argument('--to', metavar='datetime',
+                             help='Return events generated after datetime')
+            parser.add_argument('--traits', metavar='trait_list',
+                             help='Return events with specific traits.')
 
-      options:
-      --debug
-      --name <name>
-                return events of type <name>
-      --from <datetime>
-                list events generated before datetime
-      --to <datetime>
-                list events generated after datetime
-      --traits <traits>
-                list events with specific traits
-      --msg_id <message_id>
-                get event with <message_id>
+            group = parser.add_mutually_exclusive_group()
+            group.add_argument('--count', action='store_true',
+                   default=False,
+                   help='Return a count of events matching filter criteria.')
+            group.add_argument('--msg_id', metavar='message_id',
+                   help='Return a single specific event by message id.')
 
-      Trait format:
-      "trait:value;trait:value;..."
-    """
 
-    def cmdline(self, version, cmdline):
-        arguments = docopt(Events.__doc__, argv=cmdline)
-        debug = version.base_args['--debug']
-        if debug:
-            print arguments
+    def get_events_count(self, from_datetime=None, to_datetime=None,
+                         traits=None, name=None, debug=False):
+        if traits:
+            traits = ",".join(["%s:%s" % item for item in traits.items()])
+        cmd = "events/count"
+        params = base.remove_empty({'from_datetime': from_datetime,
+                                    'to_datetime': to_datetime,
+                                    'event_name': name,
+                                    'traits': traits})
 
-        response = self.do_event(version, arguments)
-        raw_rows = response.json(object_hook=jsonutil.object_hook)
+        return base.get(self.url, cmd, params, debug=debug)
 
-        if isinstance(raw_rows, dict):
-            raw_rows = [raw_rows]
+    def get_events(self, from_datetime=None, to_datetime=None,
+                  traits=None, name=None, msg_id=None, debug=False):
+        if msg_id:
+            return base.get(self.url, "events/%s" % msg_id, {}, debug=debug)
+
+        if traits:
+            traits = ",".join(["%s:%s" % item for item in traits.items()])
+        cmd = "events"
+        params = base.remove_empty({'from_datetime': from_datetime,
+                                    'to_datetime': to_datetime,
+                                    'event_name': name,
+                                    'traits': traits})
+
+        return base.get(self.url, cmd, params, debug=debug)
+
+    def _cmdline(self, arguments):
+        _from = arguments._from
+        _to = arguments.to
+        _name = arguments.name
+        _traits = arguments.traits
+        _debug = arguments.debug
+
+        trait_dict = None
+        if _traits:
+            trait_pairs = _traits.split(',')
+            trait_dict = dict([tuple(item.split(':')) for item in trait_pairs])
+
+        if arguments.count:
+            rows = self.get_events_count(from_datetime=_from, to_datetime=_to,
+                                         name=_name, traits=trait_dict,
+                                         debug=_debug)
+            keys = ['count']
+            base.dump_response(keys, rows)
+            return
+
+        _msg_id = arguments.msg_id
+        rows = self.get_events(from_datetime=_from, to_datetime=_to,
+                               name=_name, traits=trait_dict,
+                               msg_id=_msg_id, debug=_debug)
+
+        if isinstance(rows, dict):
+            rows = [rows]
         keys = set()
-        for row in raw_rows:
+        for row in rows:
             keys.update(row.keys())
         keys = sorted(list(keys))
-        base.dump_response(keys, raw_rows)
-
-    def do_event(self, version, arguments):
-        _from = arguments.get('--from')
-        _to = arguments.get('--to')
-        name = arguments.get('--name')
-        traits = arguments.get('--traits')
-        msg_id = arguments.get('--msg_id')
-
-        if msg_id:
-            cmd = "events/%s" % msg_id
-            return base.get(version.base_url, cmd, {})
-
-        cmd = "events"
-        params = base.remove_empty({'from_datetime': _from,
-                                    'to_datetime': _to,
-                                    'event_name': name,
-                                    'traits': traits})
-
-        return base.get(version.base_url, cmd, params)
+        base.dump_response(keys, rows)
 
 
-class NumEvents(object):
-    """usage:
-        klugman.py num-events [options]
+class V1(object):
+    def __init__(self, url, parser):
+        subparser = parser.add_subparsers(dest='command',
+                                          help="V1 API commands")
+        self.resources = {'events': Events(url, subparser),
+                          'streams': Streams(url, subparser)}
 
-      options:
-      --debug
-      --name <name>
-                return events of type <name>
-      --from <datetime>
-                list events generated before datetime
-      --to <datetime>
-                list events generated after datetime
-      --traits <traits>
-                list events with specific traits
+    def dispatch(self, arguments):
+        # We could let argparse dispatch automatically, but I
+        # want to make this work as a library as well as a
+        # cmdline tool, so it goes to an object vs. a function.
+        cmd = arguments.command
 
-      Trait format:
-      "trait:value;trait:value;..."
-    """
+        # This shouldn't be needed, but I'm being paranoid.
+        if cmd not in self.resources:
+            print "Unknown command: '%s'" % cmd
+            return
 
-    def cmdline(self, version, cmdline):
-        arguments = docopt(NumEvents.__doc__, argv=cmdline)
-        debug = version.base_args['--debug']
-        if debug:
-            print arguments
-
-        response = self.do_event(version, arguments)
-        raw_rows = response.json(object_hook=jsonutil.object_hook)
-
-        keys = ['count']
-        base.dump_response(keys, raw_rows)
-
-    def do_event(self, version, arguments):
-        _from = arguments.get('--from')
-        _to = arguments.get('--to')
-        name = arguments.get('--name')
-        traits = arguments.get('--traits')
-
-        cmd = "events/count"
-        params = base.remove_empty({'from_datetime': _from,
-                                    'to_datetime': _to,
-                                    'event_name': name,
-                                    'traits': traits})
-
-        return base.get(version.base_url, cmd, params)
-
-
-class V1(base.Impl):
-    """usage:
-        klugman.py streams [<args>...] [options]
-        klugman.py num-streams [<args>...] [options]
-        klugman.py events [<args>...] [options]
-        klugman.py num-events [<args>...] [options]
-
-        -h, --help  show command options
-    """
-
-    def __init__(self, base_url, base_args):
-        cmds = {'streams': Streams(),
-                'num-streams': NumStreams(),
-                'events': Events(),
-                'num-events': NumEvents()}
-        super(V1, self).__init__(base_url, base_args, cmds, V1.__doc__)
+        self.resources[cmd]._cmdline(arguments)
